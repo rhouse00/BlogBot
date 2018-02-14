aa72b137d8e66534fd50c11419b850e911c339ba
rhouse00 <ryanjameshouse@gmail.com>
2018-02-13 22:23:54 -0800

    further buildout of initial commit

---
+++ start.js
@@ -0,0 +1,21 @@
+const PORT = process.env.PORT || 1234;
+
+const [major, minor] = process.versions.node.split('.').map(parseFloat);
+
+if(major < 7 || (major === 7 && minor <= 5) ) {
+    console.log('Node version is too damn old! Please upgrade to 7.6 or higher');
+    process.exit();
+}
+
+require('dotenv').config({ path: 'variables.env'});
+
+// input db connection informaiton here
+
+// import db models here!!
+// require('./models/....);
+
+const app = require('./app');
+app.set('port', PORT);
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
