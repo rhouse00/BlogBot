15b847656740347dee5d257699109089e4aaae18
rhouse00 <ryanjameshouse@gmail.com>
2018-02-18 21:52:51 -0800

    adds pug to package for view use

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const path = require('path');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const promisify = require('es6-promisify');
 const app = express();
 
 const routes = require('./routes/routes');
@@ -16,6 +20,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: process.env.SECRET,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({mongooseConnection: mongoose.connection})
+}))
 
 app.use('/', routes);
 module.exports = app;
