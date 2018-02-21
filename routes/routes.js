34c032f365a25990b65532edda02055a17c2a6aa
rhouse00 <ryanjameshouse@gmail.com>
2018-02-20 21:16:55 -0800

    adds edit post and add post routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,47 +1,50 @@
 const express = require('express');
 const router = express.Router();
 
+const postController = require('../controllers/postController');
+const { catchErrors } = require('../handlers/errorHandlers');
+
 // SITE VISITOR
 
 // landing page
-router.get('/', function (){
-    console.log('yippie');
-} );
+router.get('/', catchErrors(postController.getPosts) );
 
 // all posts
-router.get('/posts', function (){
-    console.log('yippie');
-} );
+router.get('/posts', catchErrors(postController.getPosts) );
 
-// individual post
-router.get('/posts/:slug', function (){
-    console.log('yippie');
-} );
+// all posts paginated
+router.get('/posts/page/:page', catchErrors(postController.getPosts) );
 
-// ADMIN pages
+// // individual post
+// router.get('/posts/:slug', function (){
+//     console.log('yippie');
+// } );
 
-// landing page
-router.get('/admin', function (){
-    console.log('yippie');
-} );
+// // ADMIN pages
 
-// post creation
-router.get('/admin/new', function (){
-    console.log('yippie');
-} );
-router.post('/admin/new', function (){
-    console.log('yippie');
-} );
+// // landing page
+// router.get('/admin', function (){
+//     console.log('yippie');
+// } );
 
-// all posts
-router.get('/admin/posts', function (){
-    console.log('yippie');
-} );
+// // post creation
+// router.get('/admin/new', function (){
+//     console.log('yippie');
+// } );
+// router.post('/admin/new', function (){
+//     console.log('yippie');
+// } );
+
+// // all posts
+// router.get('/admin/posts', function (){
+//     console.log('yippie');
+// } );
+
+// // add post
+router.get('/admin/add', catchErrors(postController.addPost));
 
 // edit post
-router.get('/admin/post/:id', function (){
-    console.log('yippie');
-} );
+router.get('/admin/post/:id', catchErrors(postController.editPost) );
 router.post('/admin/post/:id', function (){
     console.log('yippie');
 } );
