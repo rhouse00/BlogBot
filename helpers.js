8ed8299771507c14a8f93e504d3ded62cfc22035
rhouse00 <ryanjameshouse@gmail.com>
2018-02-23 22:51:31 -0800

    global objects & funcs

---
+++ helpers.js
@@ -0,0 +1,7 @@
+exports.moment = require('moment');
+
+exports.siteNavMenu = [
+   {title: 'Admin Posts', route: '/admin/posts' },
+   {title: 'Add Post', route: '/admin/add'},
+   {title: 'Posts', route: '/posts'}
+];
