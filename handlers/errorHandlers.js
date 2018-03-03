9a883722c79a6f44695b16808c18509d76372522
rhouse00 <ryanjameshouse@gmail.com>
2018-03-02 21:57:49 -0800

    adds error handling and display

--- handlers/errorHandlers.js
+++ handlers/errorHandlers.js
@@ -4,3 +4,36 @@ exports.catchErrors = (fn) => {
         return fn(req, res, next).catch(next);
     };
 };
+
+exports.flashValidationErrors = (err, req, res, next) => {
+    if (!err.errors) return next(err);
+    // validation errors look like
+    const errorKeys = Object.keys(err.errors);
+    errorKeys.forEach(key => req.flash('error', err.errors[key].message));
+    res.redirect('back');
+  };
+
+exports.developmentErrors = (err, req, res, next) => {
+    err.stack = err.stack || '';
+    const errorDetails = {
+        message: err.message,
+        status: err.status,
+        stackHighlighted: err.stack.replace(/[a-z_-\d]+.js:\d+:\d+/gi, '<mark>$&</mark>')
+    };
+    res.status(err.status || 500);
+    res.format({
+        'text/html': () => {
+            res.render('error', errorDetails);
+          },
+          'application/json': () => 
+            res.json(errorDetails)
+    });
+};
+
+exports.productionErrors = (err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {}
+    });
+};
