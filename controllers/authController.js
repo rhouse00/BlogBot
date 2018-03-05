bfa16662559e12cfcb2bec8575c22eb5bd6047c1
rhouse00 <ryanjameshouse@gmail.com>
2018-03-04 22:15:18 -0800

    adds logout fn and checks if logged in

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,3 +11,18 @@ exports.login = passport.authenticate('local', {
    successRedirect: '/',
    successFlash: 'Login Successful'
 });
+
+exports.isLoggedIn = (req, res, next) => {
+   if(req.isAuthenticated()) {
+      next();
+      return;
+   }
+   req.flash('error','Please login to access that page');
+   res.redirect('/login');
+}
+
+exports.logout = (req, res) => {
+   req.logout();
+   req.flash('success', 'Successfully logged out');
+   res.redirect('/')
+}
