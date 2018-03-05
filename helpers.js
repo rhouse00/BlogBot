0b66fed80c64deea53ca6fcd621685a4018aa938
rhouse00 <ryanjameshouse@gmail.com>
2018-03-04 22:16:54 -0800

    adds logout to nav menu

--- helpers.js
+++ helpers.js
@@ -5,5 +5,6 @@ exports.siteNavMenu = [
    {title: 'Add Post', route: '/admin/add'},
    {title: 'Posts', route: '/posts'},
    {title: 'Admin Console', route: '/admin'},
-   {title: 'Register', route: '/register'}
+   {title: 'Register', route: '/register'},
+   {title: 'Logout', route: '/logout'}
 ];
