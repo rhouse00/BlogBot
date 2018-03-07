7720ff44d682527de5097799c3da26ccd718e6ec
rhouse00 <ryanjameshouse@gmail.com>
2018-03-06 21:26:59 -0800

    adds a slug and pre-populating the author from the logged in user

--- models/Post.js
+++ models/Post.js
@@ -13,14 +13,39 @@ const postSchema = new mongoose.Schema({
         trim: true,
         required: 'Please insert / add content to your post'
     },
+    slug: String,
     author: {
-        type: String,
-        trim: true
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: 'Please supply an author'
     },
     created: {
         type: Date,
         default: Date.now
     }
 });
+
+postSchema.pre('save', async function(next) {
+    if(!this.isModified('name')) {
+        next();
+        return;
+    }
+    this.slug = slug(this.name);
+    const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*)?)$`, 'i');
+    const postWithRegEx = await this.constructor.find({slug: slugRegEx });
+    if(postWithRegEx.length) {
+        this.slug = `${this.slug}-${postWithRegEx.length+1}`;
+    }
+    next();
+});
+
+function autopopulate(next) {
+    this.populate('author', 'name');
+    next();
+}
+
+postSchema.pre('find', autopopulate);
+postSchema.pre('findOne', autopopulate);
+
 
 module.exports = mongoose.model('Post', postSchema);
