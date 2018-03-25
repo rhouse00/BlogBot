82d20c66225f7ca890a4fc20ea49d06d64c302a3
rhouse00 <ryanjameshouse@gmail.com>
2018-03-24 22:58:11 -0700

    refactored menu links and routes

--- helpers.js
+++ helpers.js
@@ -7,13 +7,16 @@ exports.siteNavMenu = [
    {title: 'Logout', route: '/logout'}
 ];
 
-exports.adminNavMenu = [
+exports.adminNavMenuLeft = [
    {title: 'Admin Posts', route: '/admin/posts' },
    {title: 'Add Post', route: '/admin/add'},
    {title: 'Admin Console', route: '/admin'},
    {title: 'Accounts', route: '/admin/accounts'},
-   {title: 'Posts', route: '/posts'},
+   {title: 'Posts', route: '/posts'}
+];
+
+exports.adminNavMenuRight = [
    {title: 'Register', route: '/register'},
    {title: 'Login', route: '/login'},
    {title: 'Logout', route: '/logout'}
-];
+]
