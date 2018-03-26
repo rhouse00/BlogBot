f2d7b895723da060c7ba7a6521a56ba95229a988
rhouse00 <ryanjameshouse@gmail.com>
2018-03-25 22:31:28 -0700

    eliminates unneccessary comments

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,8 @@
 const passport = require('passport');
-// const crypto = require('crypto');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const { promisify } = require('es6-promisify');
-// const mail = require('../handlers/mail'); 
+
 
 exports.login = passport.authenticate('local', {
    failureRedirect: '/login',
