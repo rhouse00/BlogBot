966a8d16ca23e8c947b77469ffd4781d92cc75e4
rhouse00 <ryanjameshouse@gmail.com>
2018-03-30 23:13:10 -0700

    adds js file to client for curation feature

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,7 @@
 import '../sass/style.scss';
 
 import sideBar from './modules/sidebar';
+import curationPicker from './modules/curationPicker';
 
 sideBar('.sidebarTab');
+curationPicker('.button_add', '.selected_list');
