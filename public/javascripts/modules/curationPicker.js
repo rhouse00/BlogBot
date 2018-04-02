ea6b167065641b901f48267c5b834a549d596b50
rhouse00 <ryanjameshouse@gmail.com>
2018-04-01 22:06:57 -0700

    refactors fn for reuse and event delegation

--- public/javascripts/modules/curationPicker.js
+++ public/javascripts/modules/curationPicker.js
@@ -1,23 +1,24 @@
 
-function curationPicker(element, targetElem) {
-   const posts = document.querySelectorAll(element);
-
-   posts.forEach((elem) => {
-      elem.addEventListener('click', (e) => {
-         e.preventDefault();
-         let child = e.target.parentNode.parentNode;
-         // let parent = e.path[3];
-         let parNode = e.target.parentNode.parentNode.parentNode;
-         console.log(child);
-         // console.log(parent);
-         console.log(parNode);
-         // console.log(targetElem);
-         
-         parNode.removeChild(child);
-         document.querySelector(targetElem).appendChild(child);
-         console.log(e.target.value);
-      })
+function curationPicker(element, targetElem, newClassName) {
+   // const posts = document.querySelectorAll(element);
+   const el = document.querySelector(element);
+   console.log(el);
+   el.addEventListener('click', (e) => {
+      e.preventDefault();
+      let child = e.target.parentNode.parentNode;
+      console.log(child);
+      let parNode = e.target.parentNode.parentNode.parentNode;
+      console.log(parNode);
+      e.target.className = newClassName;
+      parNode.removeChild(child);
+      document.querySelector(targetElem).appendChild(child);
    })
+
+   
+   // el.addEventListener('click', (e) => {
+   //    console.log(e);
+   // })
+
 }
 
 export default curationPicker;
