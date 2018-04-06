bd86a04591b0c16532ac334a40e2d976c70e2582
rhouse00 <ryanjameshouse@gmail.com>
2018-04-05 21:29:17 -0700

    cleans up code, adds spacing/indentation

--- public/javascripts/modules/curationPicker.js
+++ public/javascripts/modules/curationPicker.js
@@ -1,15 +1,16 @@
 
-function curationPicker(element, targetElement, newClass, value) {
-   if(!window.document.URL.includes('curate')) {return}
+function curationPicker(element, targetElement, newClass, newText) {
+   if(!window.document.URL.includes('curate')) {return};
+   
    const el = document.querySelector(element);
-
    el.addEventListener('click', (e) => {
       e.preventDefault();
       if(e.target.nodeName !== 'BUTTON') {return};
+      
       let child = e.target.parentNode.parentNode; // button -> td -> tr
       let parNode = e.target.parentNode.parentNode.parentNode; // button -> td -> tr -> tbody
       e.target.className = newClass;
-      e.target.innerHTML = value;
+      e.target.innerHTML = newText;
      
       parNode.removeChild(child);
       document.querySelector(targetElement).appendChild(child);
